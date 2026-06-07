3ffb2a7b3adbb83ae6283f153135c3bf92f0c5cb
luudinhmac <nguoiemcuanui@gmail.com>
2026-06-07 11:37:46 +0700

    chore(velero): temporarily downgrade targetRevision to reset stuck sync

---
+++ patch-job.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh -p 2222 -i ~/.ssh/id_ed25519_ansible macld@103.6.235.15 'kubectl patch application platform-velero -n argocd --subresource=status --type=merge -p "{\"status\":{\"operationState\":{\"phase\":\"Failed\"}}}"'
