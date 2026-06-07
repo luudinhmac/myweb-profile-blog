a3cd0af984f98209bbc8ba46518e81041b6b6588
luudinhmac <nguoiemcuanui@gmail.com>
2026-06-07 11:49:46 +0700

    chore(velero): disable automatic upgradeCRDs job to avoid compatibility issues

--- patch-job.sh
+++ patch-job.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -p 2222 -i ~/.ssh/id_ed25519_ansible macld@103.6.235.15 'kubectl patch app platform-velero -n argocd --type merge -p "{\"status\":{\"operationState\":{\"phase\":\"Failed\"}}}"'
+ssh -p 2222 -i ~/.ssh/id_ed25519_ansible macld@103.6.235.15 'kubectl patch job velero-upgrade-crds -n velero --type=merge -p "{\"metadata\":{\"finalizers\":null}}"'
