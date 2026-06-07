45a03ac87b1495fc2949f23e7f56aed179676eb0
luudinhmac <nguoiemcuanui@gmail.com>
2026-06-07 11:45:03 +0700

    fix(velero): change kubectl override tag to latest

--- patch-job.sh
+++ patch-job.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -p 2222 -i ~/.ssh/id_ed25519_ansible macld@103.6.235.15 'kubectl patch app platform-velero -n argocd --type merge -p "{\"status\":{\"operationState\":{\"phase\":\"Terminating\"}}}"'
+ssh -p 2222 -i ~/.ssh/id_ed25519_ansible macld@103.6.235.15 'kubectl patch app platform-velero -n argocd --type merge -p "{\"status\":{\"operationState\":{\"phase\":\"Failed\"}}}"'
