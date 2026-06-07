e67d10420334d2f34640773bdb60549515c38bef
luudinhmac <nguoiemcuanui@gmail.com>
2026-06-07 11:39:58 +0700

    chore(velero): temporarily disable automated sync to clear retries

--- patch-job.sh
+++ patch-job.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -p 2222 -i ~/.ssh/id_ed25519_ansible macld@103.6.235.15 'kubectl patch application platform-velero -n argocd --subresource=status --type=merge -p "{\"status\":{\"operationState\":{\"phase\":\"Failed\"}}}"'
+ssh -p 2222 -i ~/.ssh/id_ed25519_ansible macld@103.6.235.15 'kubectl patch application platform-velero -n argocd --type=merge -p "{\"status\":{\"operationState\":{\"phase\":\"Failed\"}}}"'
